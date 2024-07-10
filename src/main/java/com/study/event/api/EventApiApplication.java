a8bb70da52629d56dcfb2c506be01098aed6d662
yaejin12 <pos03167@naver.com>
2024-07-10 12:35:49 +0900

    파일 생성 및 메일 인증 코드

---
+++ src/main/java/com/study/event/api/EventApiApplication.java
@@ -0,0 +1,13 @@
+package com.study.event.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/event/api/EventApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.event.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
