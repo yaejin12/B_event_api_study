e02950872557b33287b46ef09a0f2a43a35cc4f4
yaejin12 <pos03167@naver.com>
2024-07-23 14:22:03 +0900

    서버연결

--- src/main/java/com/study/event/api/config/SecurityConfig.java
+++ src/main/java/com/study/event/api/config/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
 
                 .antMatchers(HttpMethod.PUT, "/auth/promote").hasAuthority("COMMON")
                 // 아래의 URL요청은 로그인 없이 모두 허용
-                .antMatchers("/", "/auth/**").permitAll()
+                .antMatchers("/", "/auth/**","/file/**").permitAll()
 
                 // 나머지 요청은 전부 인증(로그인) 후 진행해라
                 .anyRequest().authenticated() // 인가 설정 on
